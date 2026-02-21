074c6f2092841a70d933fa41a0f63e208991a26a
joerozvicariato287@gmail.com <joerozvicariato287@gmail.com>
2026-02-21 22:01:13 +0800

    Add SWDC Building data to campusBuildings array

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 let map;
+
+// NBSC Campus Buildings Data
+const campusBuildings = [
+    {
+        name: "SWDC Building",
+        coordinates: [8.360392119715149, 124.86762916676057],
+        pollutionLevel: "moderate",
+        description: "Main administrative offices"
+        
+    },
+]
