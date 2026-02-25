5f21a570dfdaaa005f610b028eab1e5dcd34780c
RjayBernales <rainieljevson0@gmail.com>
2026-02-26 03:06:38 +0800

    adding users table for regular users since the webapp for users and desktop for admin would be separated

--- migration/tables_schema.sql
+++ migration/tables_schema.sql
@@ -13,6 +13,16 @@ CREATE TABLE pollution_levels (
     description TEXT
 ) ENGINE=InnoDB;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
+
 -- TABLE: admin
 CREATE TABLE admins (
     id INT AUTO_INCREMENT PRIMARY KEY,
