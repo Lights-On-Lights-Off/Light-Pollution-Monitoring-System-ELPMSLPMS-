d6ca74d609c39ae49bdd85437f57991b44f55cc3
RjayBernales <rainieljevson0@gmail.com>
2026-02-26 02:46:54 +0800

    Adding the tbl for pollution_levels

--- migration/tables_schema.sql
+++ migration/tables_schema.sql
@@ -4,3 +4,11 @@ CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 USE environmental_light_pollution_db;
+
+-- Creating TABLE pollution_levels
+CREATE TABLE pollution_levels (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    level_name VARCHAR(50) NOT NULL UNIQUE,
+    color_code VARCHAR(20) NOT NULL,
+    description TEXT
+) ENGINE=InnoDB;
