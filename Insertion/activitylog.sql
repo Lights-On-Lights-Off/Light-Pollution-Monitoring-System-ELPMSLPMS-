3a6cfcbecd19fd3f99644f78494b1689235ce188
khaiky <samanthagabitano015@gmail.com>
2026-02-26 14:55:49 +0800

    added some changes in the schema

---
+++ Insertion/activitylog.sql
@@ -0,0 +1,9 @@
+INSERT INTO activity_logs 
+(admin_id, action_type, target_table, target_id, description)
+VALUES
+(1, 'ADD_LOCATION', 'locations', 1, 
+ 'Added new location: NBSC testloc BA Building'),
+
+(1, 'ADD_LOCATION', 'locations', 2, 
+ 'Added new location: NBSC testloc SAS Office'),
+
