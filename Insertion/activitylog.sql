27cfef7074587f0c3e7fda36e22480de9ba0aed9
khaiky <samanthagabitano015@gmail.com>
2026-02-26 14:57:20 +0800

    added location, update status and resolve feedback in schema

--- Insertion/activitylog.sql
+++ Insertion/activitylog.sql
@@ -7,3 +7,11 @@ VALUES
 (1, 'ADD_LOCATION', 'locations', 2, 
  'Added new location: NBSC testloc SAS Office'),
 
+(2, 'ADD_LOCATION', 'locations', 3, 
+ 'Added new location: NBSC testloc ICS Dept'),
+
+(1, 'UPDATE_STATUS', 'locations', 1, 
+ 'Changed pollution level to High'),
+
+(1, 'RESOLVE_FEEDBACK', 'feedback', 3, 
+ 'Marked feedback as resolved');
