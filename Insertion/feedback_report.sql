25590c56179b0551a54526bd808824c7a44ae22e
RJ Bernales <rainieljevson0@gmail.com>
2026-03-09 07:23:40 +0800

    Delete Insertion directory
    Not needed

--- Insertion/feedback_report.sql
+++
@@ -1,5 +0,0 @@
-USE environmental_light_pollution_db;
-
-INSERT INTO feedback (location_id, user_id, message, status) VALUES
-(1, 1, 'The lights here are too bright even at midnight.', 'Pending'),
-(2, 2, 'Lighting is acceptable but could be reduced.', 'Reviewed');
